e37f1e6b2d9c4c1524aba9c012a5d09957d428f1
Abdulhamit EFE <69138481+hamitefe@users.noreply.github.com>
2024-07-11 16:45:55 +0300

    project

---
+++ dynamic_array.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+	size_t* sizes;
+	size_t total_size;
+	size_t item_count;
+	unsigned char* ptr;
+} DynamicArray;
+
+static DynamicArray initialize_arr() {
+
+	DynamicArray arr = {
+		NULL,
+		0,
+		0,
+		NULL
+	};
+	return arr;
+}
+//size is the size of the variable you are adding
+//ptr is the pointer to the variable
+//example usage:
+//double d = 3.0;
+//pushdarr(&arr, &d, sizeof(double))
+static void pushdarr(DynamicArray *array, unsigned char * ptr, size_t size) {
+	if (array->ptr == NULL) {
+		array->ptr = malloc(size);
+		for (int i = 0; i < size; i++) {
+			array->ptr[i] = ptr[i];
+		}
+	}
+	else {
+		array->ptr = realloc(array->ptr, array->total_size + size);
+		for (int i = 0; i < size; i++) {
+			array->ptr[array->total_size + i] = ptr[i];
+		}
+	}
+	if (array->sizes == NULL) {
+		array->sizes = malloc(sizeof(size_t));
+	}else {
+		array->sizes =(unsigned char*) realloc(array->sizes, (array->item_count+1)* + sizeof(size_t));
+	}
+	array->sizes[array->item_count] = size;
+	array->item_count++;
+	array->total_size += size;
+}
+static void readdarr(DynamicArray* array, unsigned char* ptr, int index) {
+	size_t until_item = 0;
+	if (index > 0){
+		for (int i = 0; i < index; i++) {
+			until_item += array->sizes[i];
+		}
+	}
+	for (int i = 0; i < array -> sizes[index]; i++){
+		ptr[i] = array->ptr[until_item + i];
+	}
+}
+
+static void deletedarr(DynamicArray* array, int index) {
+	if (array->item_count <= index) return;
+	if (index == array->item_count - 1) {
+		array->ptr = realloc(array->ptr, array->total_size - array->sizes[index]);
+		array->total_size = array->total_size - array->sizes[index];
+		array->item_count--;
+		array->sizes = realloc(array->sizes, array->item_count * sizeof(size_t));
+		return;
+	}
+	size_t until_item = 0;
+	for (int i = 0; i < index; i++) {
+		until_item += array->sizes[i];
+	}
+	size_t size_of_removed_item = array->sizes[index];
+	for (int i = until_item + size_of_removed_item; i < array->total_size; i++) {
+		array->ptr[i - size_of_removed_item] = array->ptr[i];
+	}
+	array->total_size = array->total_size - array->sizes[index];
+
+	for (int i = index; i < array->item_count; i++) {
+		array->sizes[index] = array->sizes[index+1];
+	}
+
+	array->item_count--;
+	array->ptr = realloc(array->ptr, array->total_size);
+
+}
---
+++ example.c
@@ -0,0 +1,43 @@
+#include "dynamic_array.c"
+typedef struct {
+	int x;
+	double y;
+	float z;
+} TestStruct;
+
+void print_struct(TestStruct data) {
+	printf("x of the struct: %d\n", data.x);
+	printf("y of the struct: %f\n", data.y);
+	printf("z of the struct: %f\n", data.z);
+}
+
+int main() {
+	DynamicArray arr = initialize_arr();
+	double double_ = 3.0;
+	int integer = 5;
+	float float_ = 10.0;
+	TestStruct data = {
+		15,
+		35.0,
+		99.0
+	};
+	pushdarr(&arr, &double_, sizeof(double));
+	pushdarr(&arr, &integer, sizeof(int));
+	pushdarr(&arr, &float_, sizeof(float));
+	pushdarr(&arr, &data, sizeof(TestStruct));
+	deletedarr(&arr, 2);
+	double readed_double = 0.0;
+	int readed_integer = 0;
+	float readed_float = 0.0;
+	TestStruct readed_struct;
+	
+	readdarr(&arr, &readed_double, 0);
+	readdarr(&arr, &readed_integer, 1);
+	readdarr(&arr, &readed_struct, 2);
+	
+	printf("readed double: %f \n", readed_double);
+	printf("readed integer: %d \n", readed_integer);
+	printf("readed struct: \n");
+	print_struct(readed_struct);
+	return 0;
+}
